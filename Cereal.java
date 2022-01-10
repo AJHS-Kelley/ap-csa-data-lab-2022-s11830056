8607dab75f9f99713d2fb618a54cd36940a1f3ca
Johann B <s11830056@students.duvalschools.org>
2022-01-10 09:32:17 -0500

    v0.2

--- Cereal.java
+++ Cereal.java
@@ -1,4 +1,4 @@
-//Johann Burke, 1/10/2022, 9:11, AP Data Lab, v0.1
+//Johann Burke, 1/10/2022, 9:11, AP Data Lab, v0.2
 public class Cereal
 {
 private String cerealName;
@@ -6,8 +6,20 @@ private double fiber;
 private double cups;
 private double fiberPerCup;
 
+public Cereal(String cerealName, double fiber, double cups, double fiberPerCup)
+{
+    name = cerealName;
+    fiber = fiber;
+    cups = cups;
+    FPC = fiberPerCup;
 
-
-
+}
+public Cereal()
+{
+    name = "";
+    fiber = 0.0;
+    cups = 0.0;
+    FPC = 0.0;
+}
 
 }
